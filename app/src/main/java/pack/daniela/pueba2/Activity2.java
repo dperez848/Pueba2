3f6a41c12f600cfee82d19168f2147239aacbba3
DANIELA PEREZ <danyperez92@gmail.com>
2014-11-07 09:49:44 -0430

    Context Menu on Activity2 Sqlite with errors on Activity7

--- app/src/main/java/pack/daniela/pueba2/Activity2.java
+++ app/src/main/java/pack/daniela/pueba2/Activity2.java
@@ -3,7 +3,9 @@ package pack.daniela.pueba2;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -16,23 +18,23 @@ import android.widget.TextView;
 
 public class Activity2 extends Activity {
 
+    private TextView pulse3;
+    private String[] data;
+    private String opcionSeleccionada;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity2);
 
-        Spinner cmbOpciones;
-        final TextView pulse3= (TextView)findViewById(R.id.center2);
-        final String[] data=new String[]{"Daniela","Daniel","Alexander","Luis","Gabriel"};
-        Button btn5=(Button)findViewById(R.id.btn5);
-        ArrayAdapter<String> adap1=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, data);
+        pulse3 = (TextView)findViewById(R.id.center2);
+        data = new String[]{"Daniela","Daniel","Alexander","Luis","Gabriel"};
 
-        cmbOpciones = (Spinner)findViewById(R.id.CmbOpciones);
-
+        ArrayAdapter<String> adap1 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, data);
         adap1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
+        Spinner cmbOpciones = (Spinner)findViewById(R.id.CmbOpciones);
         cmbOpciones.setAdapter(adap1);
-
         cmbOpciones.setOnItemSelectedListener(
                 new AdapterView.OnItemSelectedListener() {
                     public void onItemSelected(AdapterView<?> parent,
@@ -44,52 +46,66 @@ public class Activity2 extends Activity {
                         pulse3.setText("");
                     }
                 });
-        AdaptadorTitulares adaptador =  new AdaptadorTitulares(this);
 
         ListView lstOpciones = (ListView)findViewById(R.id.LstOpciones);
-
+        AdaptadorTitulares adaptador =  new AdaptadorTitulares(this);
         lstOpciones.setAdapter(adaptador);
-
         lstOpciones.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> a, View v, int position, long id) {
 
-                String opcionSeleccionada =
-                        ((Titular)a.getAdapter().getItem(position)).getTitulo();
-
-
-               pulse3.setText("Opción seleccionada: " + opcionSeleccionada);
+                opcionSeleccionada = ((Titular)a.getAdapter().getItem(position)).getTitulo();
+                pulse3.setText("Opción seleccionada: " + opcionSeleccionada);
             }
         });
 
+        Button btn5 =(Button)findViewById(R.id.btn5);
         btn5.setOnClickListener(new View.OnClickListener()
         {   public void onClick(View arg0) {
                 Intent intent = new Intent(Activity2.this, Activity3.class);
                 startActivity(intent);
-
             }
         });
+
+        registerForContextMenu(lstOpciones);
     }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_activity2, menu);
         return true;
     }
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo)
+    {
+        super.onCreateContextMenu(menu, v, menuInfo);
 
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_ctx_lista, menu);
+
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        AdapterView.AdapterContextMenuInfo info =
+                (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        switch (item.getItemId()) {
+
+            case R.id.CtxLst1Opc1:
+                pulse3.setText("Lista[" + info.position + "]: Opcion 1 pulsada!");
+                return true;
+            case R.id.CtxLst1Opc2:
+                pulse3.setText("Lista[" + info.position + "]: Opcion 2 pulsada!");
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
--- app/src/main/java/pack/daniela/pueba2/Activity7.java
+++ app/src/main/java/pack/daniela/pueba2/Activity7.java
@@ -1,39 +1,98 @@
 package pack.daniela.pueba2;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class Activity7 extends Activity {
+    private ContentValues nuevoRegistro = new ContentValues();
+    private Button show=(Button)findViewById(R.id.show);
+    private TextView txtResultado=(TextView)findViewById(R.id.results);
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity7);
+
+        //Abrimos la base de datos 'DBUsuarios' en modo escritura
+        UsuariosSQLiteHelper usdbh =
+                new UsuariosSQLiteHelper(this, "DBUsuarios", null, 1);
+
+        db = usdbh.getWritableDatabase();
+        //Si hemos abierto correctamente la base de datos
+        if(db != null)
+        {
+            //Insertamos 5 usuarios de ejemplo
+            for(int i=1; i<=5; i++)
+            {
+                //Generamos los datos
+
+                nuevoRegistro.put("codigo", i);
+                nuevoRegistro.put("nombre","Usuario" + i);
+                db.insert("Usuarios", null, nuevoRegistro);
+            }
+            //Establecemos los campos-valores a actualizar
+            ContentValues valores = new ContentValues();
+            valores.put("nombre","usunuevo");
+
+            //Actualizamos el registro en la base de datos
+            db.update("Usuarios", valores, "codigo=2", null);
+
+            //Eliminamos el registro del usuario '6'
+            db.delete("Usuarios", "codigo=3", null);
+
+            //Cerramos la base de datos
+            db.close();
+        }
+
+        Button show=(Button)findViewById(R.id.show);
+        show.setOnClickListener(new View.OnClickListener()
+        {   public void onClick(View arg0)
+            {
+                String[] campos = new String[] {"codigo", "nombre"};
+                String[] args = new String[] {"usu1"};
+
+                Cursor c = db.query("Usuarios", campos, "usuario=?", args, null, null, null);
+                //Recorremos los resultados para mostrarlos en pantalla
+                txtResultado.setText("");
+                //Nos aseguramos de que existe al menos un registro
+                if (c.moveToFirst()) {
+                    //Recorremos el cursor hasta que no haya más registros
+
+                    while(c.moveToNext()){
+                        String codigo= c.getString(0);
+                        String nombre = c.getString(1);
+                        txtResultado.append(" " + codigo + " - " + nombre + "\n");
+                    }
+
+                }
+            }
+        });
     }
+
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_activity7, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
